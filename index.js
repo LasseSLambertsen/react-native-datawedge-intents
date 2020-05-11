e26dcc95e8dd5f8774eb4acef05aa1460e84c176
LasseSLambertsen <LasseSLambertsen@users.noreply.github.com>
2020-05-11 08:22:09 +0200

    Added conditional to fix crash on unsupported devices

--- index.js
+++ index.js
@@ -4,14 +4,17 @@
 
 'use strict';
 
-var { Platform, NativeModules } = require('react-native');
-var RNDataWedgeIntents = NativeModules.DataWedgeIntents;
+let { Platform, NativeModules } = require('react-native');
+let RNDataWedgeIntents = NativeModules.DataWedgeIntents;
+let DataWedgeIntents = {};
 
-var DataWedgeIntents = {
-    //  Specifying the DataWedge API constants in this module is deprecated.  It is not feasible to stay current with the DW API.
-    registerReceiver(action, category ) {
-        RNDataWedgeIntents.registerReceiver(action,category);
-    },
-};
+if(RNDataWedgeIntents) {
+    DataWedgeIntents = {
+        //  Specifying the DataWedge API constants in this module is deprecated.  It is not feasible to stay current with the DW API.
+        registerReceiver(action, category ) {
+            RNDataWedgeIntents.registerReceiver(action,category);
+        },
+    };
+}
 
 module.exports = DataWedgeIntents;
